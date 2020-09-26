fbe1828f1a5d068d846deaee7439e18fe864e129
suneel <sunee@DESKTOP-KNHHK0Q(sksolanki531@gmail.com)>
2020-09-26 17:31:37 +0500

    Add project files.

---
+++ JokesApplication/Models/Joke.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesApplication.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+
+    }
+}
